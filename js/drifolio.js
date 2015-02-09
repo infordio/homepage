ebc1141c6534f1ab544450551f739bc4f0093f4c
infordiosterada <s_terada@192.168.120.145>
2015-02-09 14:42:10 +0900

    初回コミットテスト

--- js/drifolio.js
+++ js/drifolio.js
@@ -23,53 +23,53 @@ $("html").niceScroll({
 //========================
 //SMOOTHSCROLL
 //========================
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+//$(function() {
+//  $('a[href*=#]:not([href=#])').click(function() {
+//    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//      var target = $(this.hash);
+//      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//      if (target.length) {
+//        $('html,body').animate({
+//          scrollTop: target.offset().top
+//        }, 1000);
+//        return false;
+//      }
+//    }
+//  });
+//});
 
 
 //========================
 //NAVBAR
 //========================
-(function ($) {
-  $(document).ready(function(){
-
-    // hide .navbar first
-    $(".navbar").hide();
-
-    // fade in .navbar
-    $(function () {
-        $(window).scroll(function () {
-
-                 // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 40) {
-                $('.navbar')
-                .removeClass('animated fadeOutUp')
-                .addClass('animated fadeInDown')
-                .fadeIn();
-                
-            } else {
-                $('.navbar')
-                .removeClass('animated fadeInDown')
-                .addClass('animated fadeOutUp')
-                .fadeOut();
-            }
-        });
-    });
-
-});
-  }(jQuery));
+//(function ($) {
+//  $(document).ready(function(){
+//
+//    // hide .navbar first
+//    $(".navbar").hide();
+//
+//    // fade in .navbar
+//    $(function () {
+//        $(window).scroll(function () {
+//
+//                 // set distance user needs to scroll before we start fadeIn
+//            if ($(this).scrollTop() > 40) {
+//                $('.navbar')
+//                .removeClass('animated fadeOutUp')
+//                .addClass('animated fadeInDown')
+//                .fadeIn();
+//                
+//            } else {
+//                $('.navbar')
+//                .removeClass('animated fadeInDown')
+//                .addClass('animated fadeOutUp')
+//                .fadeOut();
+//            }
+//        });
+//    });
+//
+//});
+//  }(jQuery));
 
 //========================
 //icon hover effect
